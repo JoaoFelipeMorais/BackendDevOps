7cf5499cec54cb78de0b53bddebee80712c20de5
JOÃO FELIPE MORAIS (2508893) <2508893@edu.anchieta.br>
2026-08-24 21:21:40 -0300

    estruturando a api e adicionando DB com sequelize e ORM

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 // importa o express ao arquivo
-import express, {Request, Response} from "express";
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes";
 
 //instancia o express em app e define em qual porta vai rodar a API
 const app = express();
 const PORT = 3000;
 
 //define que a linguagem da API, no caso JSON
-app .use(express.json());
+app.use(express.json());
+app.use("/api/usuarios", usuarioRoutes);
 
-// executa um get para o path(caminho) e arry function
-app.get("/api/usuario", (req: Request, res: Response)  => {
-    res.json({"materia": "devops backend"});
-});
 
 app.listen(PORT, () => {
     console.log("Servidor rodando, parmera não tem mundial");
